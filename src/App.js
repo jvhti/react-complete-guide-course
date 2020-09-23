032897e24bcb492dd5f16dfe9d6270c6a17192a3
jvhti <jvhti@hotmail.com>
2020-09-23 19:24:38 -0300

    Create a functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     // JSX:
-    // return (
-    //   <div className="App">
-    //       <h1>Hi, I'm a React App</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person/>
+      </div>
+    );
 
     // "Compiled code":
-    return React.createElement('div', {className: 'App'},
-        React.createElement('h1', null, 'Hi, I\'m a React App'));
+    //return React.createElement('div', {className: 'App'},
+    //    React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
 }
 
