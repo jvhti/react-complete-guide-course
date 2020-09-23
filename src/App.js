9d8f56ae7e5d5acf1b4ebed396283adbeda5f29e
jvhti <jvhti@hotmail.com>
2020-09-23 19:16:59 -0300

    Example of JSX compiled

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-          <h1>Hi, I'm a React App</h1>
-      </div>
-    );
+    // JSX:
+    // return (
+    //   <div className="App">
+    //       <h1>Hi, I'm a React App</h1>
+    //   </div>
+    // );
+
+    // "Compiled code":
+    return React.createElement('div', {className: 'App'},
+        React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
 }
 
