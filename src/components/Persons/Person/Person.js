72df68745d205217f63a33c2c13267850c7a79e3
jvhti <jvhti@hotmail.com>
2020-09-24 19:59:44 -0300

    Using React's own "Aux" component (React.Fragment)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
-import Aux from "../../../hoc/Aux";
 
 class Person extends Component{
   render() {
     console.log('[Person.js] rendering...');
     return (
-        <Aux>
+        <React.Fragment>
           <div className={classes.Person}>
             <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
             <p>{this.props.children}</p>
             <input type="text" onChange={this.props.changed} value={this.props.name}/>
           </div>
           <div style={{display: 'none'}}>On the same level of person</div>
-        </Aux>
+        </React.Fragment>
     );
   }
 }
