783e27e31b9ebac9fb921521a720ebb4abc57768
jvhti <jvhti@hotmail.com>
2020-09-23 21:49:43 -0300

    Conditionally rendering content (another way)

--- src/App.js
+++ src/App.js
@@ -45,23 +45,27 @@ class App extends Component {
       cursor: 'pointer'
     };
 
-    // JSX:
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+          <div>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}
+                    click={this.switchNameHandler.bind(this, "Called from paragraph")}/>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}>
+              My Hobbies: Racing
+            </Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button style={buttonStyle} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {
-          this.state.showPersons ?
-            <div>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}
-                         click={this.switchNameHandler.bind(this, "Called from paragraph")}/>
-              <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}>
-                My Hobbies: Racing
-              </Person>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-            </div> : null
-        }
+        {persons}
       </div>
     );
 
