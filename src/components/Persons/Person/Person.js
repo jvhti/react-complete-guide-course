0e495bd85da8455380558c5b748c9534ecca4fd4
jvhti <jvhti@hotmail.com>
2020-09-24 19:57:59 -0300

    Rendering adjacent (on root element) JSX with a HOC Component.
    I hid the adjacent element, but it's still there.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from "../../../hoc/Aux";
 
 class Person extends Component{
   render() {
     console.log('[Person.js] rendering...');
     return (
-        <div className={classes.Person}>
-          <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-          <p>{this.props.children}</p>
-          <input type="text" onChange={this.props.changed} value={this.props.name}/>
-        </div>
+        <Aux>
+          <div className={classes.Person}>
+            <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+            <p>{this.props.children}</p>
+            <input type="text" onChange={this.props.changed} value={this.props.name}/>
+          </div>
+          <div style={{display: 'none'}}>On the same level of person</div>
+        </Aux>
     );
   }
 }
