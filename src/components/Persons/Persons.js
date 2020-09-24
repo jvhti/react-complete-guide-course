31214d571c04c805f9013ac6995701f0ccc5f41e
jvhti <jvhti@hotmail.com>
2020-09-24 19:16:04 -0300

    Logging component update lifecycle (for props change)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,6 +2,25 @@ import React, {Component} from 'react';
 import Person from "./Person/Person";
 
 class Persons extends Component {
+    // static getDerivedStateFromProps(props, state){
+    //     console.log('[Persons.js] getDerivedStateFromProps', props. state);
+    //     return state;
+    // }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log('[Persons.js] shouldComponentUpdate', nextProps, nextState, nextContext);
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate', prevProps, prevState);
+        return {snapshot: 'test'};
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate', prevProps, prevState, snapshot);
+    }
+
     render() {
         console.log('[Persons.js] rendering...');
         return this.props.persons.map(
