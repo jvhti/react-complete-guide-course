b818d96246eae166f5e9c950e694c2b6b001b1b8
jvhti <jvhti@hotmail.com>
2020-09-23 21:46:22 -0300

    Conditionally rendering content

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 26}
-    ]
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -31,6 +32,10 @@ class App extends Component {
     });
   };
 
+  togglePersonsHandler = () => {
+    this.setState({ showPersons: !this.state.showPersons });
+  };
+
   render() {
     const buttonStyle = {
       backgroundColor: 'white',
@@ -45,12 +50,18 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button style={buttonStyle} onClick={() => this.switchNameHandler("Called from App")}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this, "Called from paragraph")}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}>
-          My Hobbies: Racing
-        </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button style={buttonStyle} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {
+          this.state.showPersons ?
+            <div>
+              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}
+                         click={this.switchNameHandler.bind(this, "Called from paragraph")}/>
+              <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}>
+                My Hobbies: Racing
+              </Person>
+              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div> : null
+        }
       </div>
     );
 
